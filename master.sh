6616c0d10390d92f2173ed07d25bb77398538068
Animesh Rastogi <34956961+rastogiji@users.noreply.github.com>
2022-03-29 01:38:52 +0530

    Initial Commit

---
+++ master.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+gcloud config set project $1
+destination=$2
+
+gcloud compute instances list --filter="name~'test*'" --format="table(name,zone.basename())" > details.txt
+
+arr=()
+for line in `cat details.txt`
+do
+    if [ $line != "NAME:" ] && [ $line != "ZONE:" ]
+    then
+        arr+=("$line")
+    fi
+done
+for (( i=0; i<${#arr[@]}; i+=2 ));
+do
+    ( ./move.sh ${arr[i]} ${arr[i + 1]} $destination & );
+done
+
+rm details.txt
+
+
