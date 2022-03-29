fb66dd8c31d57adee4943f590979eb51cb40bac7
Animesh Rastogi <34956961+rastogiji@users.noreply.github.com>
2022-03-29 14:25:49 +0530

    Update Image project

--- move.sh
+++ move.sh
@@ -5,8 +5,8 @@ gcloud compute snapshots create $1 \
 gcloud compute images create $1 \
     --source-snapshot=$1 \
 
-gcloud config set project $3
+gcloud config set project $4
 gcloud compute instances create $1 \
-    --image-project concise-rune-302709 \
+    --image-project $3 \
     --image $1 --zone $2 --machine-type=e2-micro \
     --tags=http-server,https-server
